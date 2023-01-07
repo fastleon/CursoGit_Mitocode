aa1833c723cd0a7770fb5d5f3436c4054b1068f9
SpyasDev <79065468+fastleon@users.noreply.github.com>
2023-01-06 19:18:05 -0500

    Se genera un ejemplo de Spring con solo configuracion XML, se crea el paquete beans2, IEquipo2, beans2.xml y App2_XML para esta aplicacion

---
+++ STS/spring/src/main/java/com/mitocode/beans2/Barcelona.java
@@ -0,0 +1,12 @@
+package com.mitocode.beans2;
+
+import com.mitocode.interfaces.IEquipo2;
+
+public class Barcelona implements IEquipo2{
+	
+	@Override
+	public String mostrar() {
+		return "Barcelona Futbol Club";
+	}
+
+}
---
+++ STS/spring/src/main/java/com/mitocode/interfaces/IEquipo2.java
@@ -0,0 +1,7 @@
+package com.mitocode.interfaces;
+
+public interface IEquipo2 {
+	
+	public String mostrar();
+
+}
